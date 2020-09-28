c05c0c58d13a4f0d7bb482ebe3e9e91d8c5d99cb
Walt Woods <waltw@galois.com>
2020-09-28 09:53:06 -0700

    Schizo: fixed 10+ pages, more accurate RMSE

--- pdf/schizo_test/main.py
+++ pdf/schizo_test/main.py
@@ -11,23 +11,24 @@ import subprocess
 import sys
 import tempfile
 
-RMSE_FOR_SCHIZO = 10.  # Out of 255
+RMSE_FOR_SCHIZO = 2.  # Out of 255
 
 def main():
     fname = sys.argv[1]
     html_out = len(sys.argv) > 2 and sys.argv[2] == '--html'
 
     res = '100'  # DPI
+    img_attrs = 'width="400"'
     with TempDir() as dname:
         tools = ['mutool', 'pdftocairo']
 
-        subprocess.call(
+        subprocess.check_call(
                 ['mutool', 'draw', '-r', res, '-o',
                     os.path.join(dname, 'mutool-%d.png'), fname],
                 stdout=subprocess.PIPE,
                 stderr=subprocess.PIPE,
                 )
-        subprocess.call(
+        subprocess.check_call(
                 ['pdftocairo', '-png', '-r', res, fname,
                     os.path.join(dname, 'pdftocairo')],
                 stdout=subprocess.PIPE,
@@ -36,6 +37,16 @@ def main():
 
         if html_out:
             print('<!DOCTYPE html><html><title>Schizo Test</title><body><table>')
+
+        pages_by_tool = {}
+        for f in os.listdir(dname):
+            for t in tools:
+                prefix = f'{t}-'
+                if not f.startswith(prefix):
+                    continue
+                pageno = int(f[len(prefix):].split('.')[0])
+                assert (t, pageno) not in pages_by_tool, f'{t} / {pageno} / {pages_by_tool}'
+                pages_by_tool[(t, pageno)] = f
 
         pageno = 1
         while True:
@@ -47,25 +58,33 @@ def main():
                 print(f'<tr><td>Page {pageno}</td>')
 
             for t in tools:
-                pagename = f'{t}-{pageno}.png'
-                pagepath = os.path.join(dname, pagename)
-                if not os.path.lexists(pagepath):
+                pagename = pages_by_tool.get((t, pageno))
+                if pagename is None:
                     if html_out:
                         print(f'<td>{t} did not render this page</td>')
                     continue
                 existed.add(pagename)
 
+                pagepath = os.path.join(dname, pagename)
                 if html_out:
                     b64 = base64.b64encode(open(pagepath, 'rb').read())
                     b64 = b64.decode('latin1')
-                    print(f'<td><img src="data:image/png;base64,{b64}" />')
+                    print(f'<td><img src="data:image/png;base64,{b64}" {img_attrs} />')
 
-                img = imageio.imread(pagepath)
+                try:
+                    img = imageio.imread(pagepath)
+                except ValueError:
+                    if not html_out:
+                        # Report this error.
+                        print(f'Unable to read image!', file=sys.stderr)
+                        raise
+                    print(f'Unable to read image!</td>')
+                    continue
                 img = img.astype(float)
 
                 diff = None
                 if base is not None:
-                    filt_sz = 2
+                    filt_sz = 3
                     if False:
                         # Old, erode filter
                         # "diff" only exists if base has been set!
@@ -86,6 +105,25 @@ def main():
                         base_mx = scipy.ndimage.filters.maximum_filter(base,
                                 size=(filt_sz, filt_sz, 1))
 
+                        if True:
+                            # Post-processing. Box-difference with filt_sz 2
+                            # missed some alignment errors, but filt_sz 3
+                            # is overly forgiving, as some letters are less than
+                            # 2px wide. Therefore, do a soft min/max based on
+                            # current pixel value with variance explained by
+                            # local min/max.
+                            u = 0.85
+                            img_mn *= u
+                            img_mx *= u
+                            base_mn *= u
+                            base_mx *= u
+
+                            uu = 1 - u
+                            img_mn += uu * img
+                            img_mx += uu * img
+                            base_mn += uu * base
+                            base_mx += uu * base
+
                         diff = np.maximum(
                                 img_mn - base_mx,
                                 base_mn - img_mx)
@@ -97,7 +135,7 @@ def main():
                         imgdat = imageio.imwrite(buf, 255 - diff, 'png')
                         buf.seek(0)
                         b64 = base64.b64encode(buf.read()).decode('latin1')
-                        print(f'<img src="data:image/png;base64,{b64}" />')
+                        print(f'<img src="data:image/png;base64,{b64}" {img_attrs} />')
                     print(f'<br />{t}')
                 if base is None:
                     base = img
@@ -125,7 +163,7 @@ def main():
                 break
 
             if len(tools) != len(existed):
-                print('Schizophrenic: page {pageno} only existed in some tools: {existed}',
+                print(f'Schizophrenic: page {pageno} only existed in some tools: {existed}',
                         file=sys.stderr)
             pageno += 1
 
