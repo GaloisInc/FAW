13db997a16e071ee7b04fb52c7f7d7671cde5d22
Evan Sultanik <evan.sultanik@trailofbits.com>
2021-11-05 12:45:06 -0400

    Use a temporary file for the polytracker database

--- pdf/plugin-polytracker/main.py
+++ pdf/plugin-polytracker/main.py
@@ -1,21 +1,25 @@
 import os
 import subprocess
+from tempfile import NamedTemporaryFile
 
 from polytracker import PolyTrackerTrace
 
 
 def process_pdf(path: str):
-    db_path = "polytracker.db"
-    if os.path.exists(db_path):
-        os.unlink(db_path)
-    subprocess.check_call(["/usr/bin/mutool_track", "info", path], env={
-        "POLYPATH": path,
-        "POLYDB": db_path
-    })
-    trace = PolyTrackerTrace.load(db_path)
-    for event in trace:
-        # this prints every single event in the trace, in order
-        print(event)
+    with NamedTemporaryFile("wb", delete=False) as tmpfile:
+        try:
+            tmpfile.close()
+            db_path = tmpfile.name
+            subprocess.check_call(["/usr/bin/mutool_track", "info", path], env={
+                "POLYPATH": path,
+                "POLYDB": db_path
+            })
+            trace = PolyTrackerTrace.load(db_path)
+            for event in trace:
+                # this prints every single event in the trace, in order
+                print(event)
+        finally:
+            os.unlink(tmpfile.name)
 
 
 if __name__ == "__main__":
