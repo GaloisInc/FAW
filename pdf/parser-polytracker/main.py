21bfab8027cb3a49ea5994c7108179e6ca9b1af2
Evan Sultanik <evan.sultanik@trailofbits.com>
2022-04-29 19:54:40 -0400

    PolyTracker file cavities (#41)
    * Partially working PolyTracker file detail view plugin
    
    * Process PolyTracker output for the file detail view
    
    * Support for the latest PolyTracker Docker container
    
    * Use a temporary file for the polytracker database
    
    * Copy base libtinfo.so library
    
    * Started creating the file cavity parser
    
    * Update to the new version of PolyTracker
    
    * Renamed the PolyTracker parser
    
    * URL for xpdf-tools-linux-4.03.tar.gz no longer works; update to 4.04
    
    * Update to the latest version of PolyTracker with TDAG output
    
    * Use a tagged version of the PolyTracker Docker image specifically for the FAW
    
    * Update the output format to just include the content of cavities and their context
    
    * Moving timeout to FAW; using stdstar since stderr emits messages too
    
    Co-authored-by: Walt Woods <waltw@galois.com>

---
+++ pdf/parser-polytracker/main.py
@@ -0,0 +1,40 @@
+import os
+from pathlib import Path
+import sys
+import subprocess
+from tempfile import NamedTemporaryFile
+from typing import Optional
+
+from dumptdag import cavity_detection
+
+
+def process_pdf(path: str, timeout: Optional[float] = None):
+    with NamedTemporaryFile("wb", delete=False) as tmpfile, NamedTemporaryFile("wb", delete=False) as psfile:
+        try:
+            db_path = tmpfile.name
+            ps_path = psfile.name
+            tmpfile.close()
+            psfile.close()
+            subprocess.check_call(["/usr/bin/mutool_track_no_control_flow", "draw", "-o", ps_path, path], env={
+                "POLYDB": db_path
+            }, timeout=timeout)
+            cavity_detection(Path(db_path), Path(path))
+        except TimeoutError:
+            sys.stderr.write(f"`mutool draw` timed out after {timeout} seconds")
+        except subprocess.CalledProcessError as e:
+            sys.stderr.write(f"`mutool draw` exited with code {e.returncode}")
+        finally:
+            os.unlink(tmpfile.name)
+            os.unlink(psfile.name)
+
+
+if __name__ == "__main__":
+    import argparse
+
+    parser = argparse.ArgumentParser()
+    parser.add_argument("INPUT_PATH", help="path to the PDF to analyze")
+    parser.add_argument("--timeout", "-t", type=float, default=None, help="timeout in seconds")
+
+    args = parser.parse_args()
+
+    process_pdf(args.INPUT_PATH, timeout=args.timeout)
